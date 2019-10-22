2d8c669c20822d60087508a3136490d59cc8c9af
github username <github email address>
2019-10-21 21:24:20 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog
+    def name=(dog_name) #setter / writer
+        @name = dog_name
+    end
+    def name #getting / reader
+        @name
+    end
+
+    def breed=(dog_breed)
+        @breed = dog_breed
+    end
+    def breed
+        @breed
+    end
+end
