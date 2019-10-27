a86e37fec8abcc530a18aad42ea7775deb483fd9
Dwight Marie <dwightmarie@gmail.com>
2019-10-27 02:05:09 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+class Dog 
+
+  
+  def name
+    @name 
+  end
+    def name=(str)
+      @name = str
+  end
+  
+  def breed 
+   @breed 
+  end
+  
+  
+  def breed=(str)
+  @breed = str
+end
+  
+  
+  
+  
+  
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,21 @@
+class Person 
+  
+  
+  def name
+    @name
+end
+
+def name=(str)
+  @name = str
+end
+  
+  
+  
+  def job 
+    @job
+end
+
+def job=(str)
+  @job = str
+end
+end
