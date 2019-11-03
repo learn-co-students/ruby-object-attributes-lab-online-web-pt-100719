99f6ed1416848237146a164bed27437eb396871b
Asiful Choudhury <asiful307@gmail.com>
2019-11-03 19:28:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,17 @@
-class Dog 
-  def initialize(name)
-    
-  end 
-end 
+class Dog
+  def name
+    @name
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+end
